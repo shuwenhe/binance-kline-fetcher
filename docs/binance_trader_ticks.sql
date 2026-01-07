fa5070b08b304a1217ef2af65969336488821ecc
shuwen <1201220707@pku.edu.cn>
2026-01-08 01:42:23 +0800

    feat:binance kline fetcher

--- docs/binance_trader_ticks.sql
+++ docs/binance_trader_ticks.sql
@@ -1,13 +1,17 @@
-CREATE TABLE quant.binance_trades_tick
-(
-    `inst_id` String,
-    `timestamp` UInt64,
-    `trade_id` String,
-    `price` Float64,
-    `size` Float64,
-    `side` String
-)
-ENGINE = MergeTree
-ORDER BY (inst_id,
- timestamp)
-SETTINGS index_granularity = 8192;
+DROP TABLE IF EXISTS quant.binance_trades_tick;
+CREATE TABLE quant.binance_trades_tick (
+    time_stamp Int64,
+    symbol String,
+    open Float64,
+    high Float64,
+    low Float64,
+    close Float64,
+    volume Float64,
+    amount Float64,
+    trade Int32,
+    type String,
+    datetime String,
+    date String,
+    date_stamp Int64
+) ENGINE = MergeTree() 
+ORDER BY (symbol, time_stamp);
